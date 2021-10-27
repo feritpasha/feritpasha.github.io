fd2aea652b7e5c188f41896a8ede749997104f49
feritpasha <93209202+feritpasha@users.noreply.github.com>
2021-10-27 06:23:28 +0400

    Update main.js

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -1,3 +1,25 @@
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+
+
 
 let myImage = document.querySelector('img');
 
@@ -35,20 +57,3 @@ return result;
 
 console.log(multiply(5,9));
 
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-
-function setUserName() {
-  let myName = prompt('Please enter your name.');
-  if(!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'Mozilla is cool, ' + myName;
-  }
-}
-
-
-myButton.onclick = function() {
-  setUserName();
-}
