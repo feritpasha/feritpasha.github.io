504850d5fc182dd802c9a5789892fb775357e933
feritpasha <93209202+feritpasha@users.noreply.github.com>
2022-03-19 16:13:08 +0400

    Update main.js

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -1,6 +1,3 @@
-
-
-
 let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
@@ -13,29 +10,6 @@ myImage.onclick = function() {
 }
 
 
-let myVariable="BoB";
-myVariable="Steven";
-console.log(myVariable==="Bob");
-console.log(myVariable);
-console.log(!(myVariable==="Jake"));
-console.log(myVariable!=="Chris");
-let randomArray=["apple",17,true,8];
-console.log(randomArray[1]);
-
-//This is commen in single line
-/*This is also comment written in
-seperate lines*/
-
-console.log("Elon"+"Musk");
-
-
-function multiply (num1,num2){
-let result=num1*num2;
-return result; 
-
-}
-
-console.log(multiply(5,9));
 
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
